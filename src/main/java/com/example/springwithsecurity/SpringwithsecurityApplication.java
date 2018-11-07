4744a46ee9b940bdd3b99a8615a6b3fd67d89b4a
Abezer <abezes.haile@gmail.com>
2018-11-07 09:44:50 -0500

    Initial commit

---
+++ src/main/java/com/example/springwithsecurity/SpringwithsecurityApplication.java
@@ -0,0 +1,12 @@
+package com.example.springwithsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwithsecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringwithsecurityApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/springwithsecurity/SpringwithsecurityApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.springwithsecurity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringwithsecurityApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
